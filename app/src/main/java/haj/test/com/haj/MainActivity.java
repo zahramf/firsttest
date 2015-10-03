c30ec70d05e7893b75a0cd2ed001d77e344b5ff9
zahra <zahramusavifakhr@gmail.com>
2015-10-04 01:35:41 +0330

    add comment

--- app/src/main/java/haj/test/com/haj/MainActivity.java
+++ app/src/main/java/haj/test/com/haj/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //just for example
         load();
 
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.labbaik);
